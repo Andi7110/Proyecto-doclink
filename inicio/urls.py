a968532f5d304343aa0fc72c9a627ab99ec9cfdc
Zeldax15 <ZR20005@ti.ues.edu.sv>
2025-05-23 15:31:40 -0600

    Registro y principal

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import login_view
+from .views import inicio_view
+from .views import seleccion_view
 
 urlpatterns = [
-    path('', login_view, name='login'),
+    path('', inicio_view, name='inicio'),
+    path('login/', login_view, name='login'),
+    path('seleccion_registro/', seleccion_view, name='seleccion'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def inicio_view(request):
+    return render(request, 'inicio/inicial.html')
+
 def login_view(request):
     return render(request, 'inicio/login.html')
+
+def seleccion_view(request):
+    return render(request, 'inicio/seleccion_registro.html')
