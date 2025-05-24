8f7d91b361ddce7d35f94368e730017a91745f44
Zeldax15 <ZR20005@ti.ues.edu.sv>
2025-05-23 22:03:46 -0600

    Registros y vista de autenticacion

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,9 +2,29 @@ from django.urls import path
 from .views import login_view
 from .views import inicio_view
 from .views import seleccion_view
+from .views import registroDoctor_view
+from .views import registroDoctor2_view
+from .views import registroDoctor3_view
+from .views import registroDoctor4_view
+from .views import registroPaciente1_view
+from .views import registroPaciente2_view
+from .views import registroPaciente3_view
+from .views import registroPaciente4_view
+from .views import contraOlvidada_view
+from .views import autenticacion_view
 
 urlpatterns = [
     path('', inicio_view, name='inicio'),
     path('login/', login_view, name='login'),
     path('seleccion_registro/', seleccion_view, name='seleccion'),
+    path('registro_doctor/', registroDoctor_view, name='registro_doctor'),
+    path('registro_doctor2/', registroDoctor2_view, name='registro_doctor2'),
+    path('registro_doctor3/', registroDoctor3_view, name='registro_doctor3'),
+    path('registro_doctor4/', registroDoctor4_view, name='registro_doctor4'),
+    path('registro_paciente1/', registroPaciente1_view, name='registro_paciente1'),
+    path('registro_paciente2/', registroPaciente2_view, name='registro_paciente2'),
+    path('registro_paciente3/', registroPaciente3_view, name='registro_paciente3'),
+    path('registro_paciente4/', registroPaciente4_view, name='registro_paciente4'),
+    path('contra_olvidada/', contraOlvidada_view, name='contra_olvidada'),
+    path('autenticacion/', autenticacion_view, name='autenticacion'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -8,3 +8,33 @@ def login_view(request):
 
 def seleccion_view(request):
     return render(request, 'inicio/seleccion_registro.html')
+
+def registroDoctor_view(request):
+    return render(request, 'inicio/registro_docto1.html')
+
+def registroDoctor2_view(request):
+    return render(request, 'inicio/registro_docto2.html')
+
+def registroDoctor3_view(request):
+    return render(request, 'inicio/registro_docto3.html')
+
+def registroDoctor4_view(request):
+    return render(request, 'inicio/registro_docto4.html')
+
+def registroPaciente1_view(request):
+    return render(request, 'inicio/registro_paciente1.html')
+
+def registroPaciente2_view(request):
+    return render(request, 'inicio/registro_paciente2.html')
+
+def registroPaciente3_view(request):
+    return render(request, 'inicio/registro_paciente3.html')
+
+def registroPaciente4_view(request):
+    return render(request, 'inicio/registro_paciente4.html')
+
+def contraOlvidada_view(request):
+    return render(request, 'inicio/contra_olvidada.html')
+
+def autenticacion_view(request):
+    return render(request, 'inicio/autenticacion.html')
