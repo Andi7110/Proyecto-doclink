be6b7fa58753d22138e15fbc5c8a4c897bf6559e
Danield05 <joseaquino971@gmail.com>
2025-09-30 19:59:53 -0600

    Comentar ALLOWED_HOSTS y CSRF_TRUSTED_ORIGINS en settings.py para facilitar la configuración en producción

--- Donlink/settings.py
+++ Donlink/settings.py
@@ -30,12 +30,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'doclink-djangoapp.softwar.me').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'doclink-djangoapp.softwar.me').split(',')
 
 
 # CSRF Trusted Origins for production
 CSRF_TRUSTED_ORIGINS = [
-    'doclink-djangoapp.softwar.me',
+    # 'doclink-djangoapp.softwar.me',
 ]
 
 # Cookie settings for production
