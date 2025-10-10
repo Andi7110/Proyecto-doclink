2c8c9d647209eec73a3df5db060a955ed0559be7
Danield05 <joseaquino971@gmail.com>
2025-10-09 21:10:03 -0600

    Clean old document paths

--- bd/management/commands/fix_document_paths.py
+++ bd/management/commands/fix_document_paths.py
@@ -2,16 +2,14 @@ from django.core.management.base import BaseCommand
 from bd.models import ConsultaMedica
 
 class Command(BaseCommand):
-    help = 'Corrige las rutas de documentos_adjuntos que empiecen con "consultas/"'
+    help = 'Limpia rutas antiguas de documentos_adjuntos'
 
     def handle(self, *args, **options):
         updated_count = 0
-        for consulta in ConsultaMedica.objects.exclude(documentos_adjuntos__startswith='consultas/').exclude(documentos_adjuntos__isnull=True).exclude(documentos_adjuntos=''):
-            old_path = consulta.documentos_adjuntos.name
-            new_path = f"consultas/{old_path}"
-            consulta.documentos_adjuntos.name = new_path
+        for consulta in ConsultaMedica.objects.filter(documentos_adjuntos__startswith='consultas/'):
+            consulta.documentos_adjuntos = None
             consulta.save()
-            self.stdout.write(f"Updated {old_path} to {new_path}")
+            self.stdout.write(f"Limpiado documentos_adjuntos para consulta {consulta.id_consulta_medica}")
             updated_count += 1
 
-        self.stdout.write(self.style.SUCCESS(f'Se actualizaron {updated_count} rutas de documentos.'))
+        self.stdout.write(self.style.SUCCESS(f'Se limpiaron {updated_count} rutas antiguas.'))
